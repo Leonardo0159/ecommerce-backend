520b75ae9e150ed4445bfc26bba0c6f6a5942cab
Leonardo <leo.sn159@gmail.com>
2022-03-26 12:46:02 -0300

    Enviando para o GIT

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function listAll() {
+        $categories = Category::get();
+
+        return response()->json(compact('categories'));
+    }
+
+    public function getWithId(int $categoryId) {
+
+        $category = null;
+
+        $category = Category::where('id', $categoryId)->first();
+
+        return response()->json(compact('category'));
+    }
+
+    public function save(Request $request, int $categoryId = null) {
+        $this->validate($request, [
+            "name" => "required|string|max:100|min:1"
+        ]);
+
+        $name = $request->post("name");
+        if ($categoryId) {
+            $category = Category::where('id', $categoryId)->update([
+                "name" => $name
+            ]);
+        } else {
+            $category = Category::create([
+                "name" => $name
+            ]);
+        }
+    }
+
+    public function delete() {
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::prefix('admin')->group(function() {
+    Route::prefix('category')->group(function() {
+        Route::get('list', 'CategoryController@listAll');
+        Route::get('list/{id}', 'CategoryController@getWithId');
+        Route::post('save', 'CategoryController@save');
+        Route::post('save/{id}', 'CategoryController@save');
+        Route::delete('delete', 'CategoryController@delete');
+    });
+});
